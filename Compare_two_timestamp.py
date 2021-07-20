137ada70212efc6724e1c553ef0fb1ea3f4a839d
Ramshiv7 <67915300+Ramshiv7@users.noreply.github.com>
2021-07-20 16:22:16 +0530

    reformat

--- Compare_two_timestamp.py
+++ Compare_two_timestamp.py
@@ -1,5 +1,5 @@
-x = 30
-format = '%Y%m%d%H%M%S'
+x = 30 ### Get input from Metadata
+format = '%Y%m%d%H%M%S' ### required Output format 
 
 std_del = '20210720152706' ###### Start Delta to get from Shell output 
 yy = int(std_del[:4])
@@ -9,31 +9,30 @@ H = int(std_del[8:10])
 M = int(std_del[10:12])
 S = int(std_del[12:14])
 
+## TO get current timestamp Value 
 from datetime import datetime
-ct_time = datetime.now()   #### TO get current timestamp Value 
+ct_time = datetime.now()   
 ct_time = ct_time.strftime('%Y-%m-%d %H:%M:%S')
 ct_timestamp = datetime.strptime(ct_time,'%Y-%m-%d %H:%M:%S') ## Current Timestamp Value 
-
 print("Current Time is : ", ct_timestamp)
 
+### Converting Start delta string into Python Date & Time object
 import datetime
 start_del_val = datetime.datetime(yy, mth, dt, H, M, S)
-
 print("Start Detla is: ", start_del_val)
 
+### New HWM Value 
 time_change = datetime.timedelta(hours = x)
-
 new_hwm_chge = start_del_val + time_change
-
 print("New HWM :",new_hwm_chge)
 
-diff = new_hwm_chge - ct_timestamp
 
+### HWM & Current timestamp comparison 
 if new_hwm_chge >= ct_timestamp :
-    print("HWM is greater than current timestamp, Assigning currnt timestamp")
-    start_delta = ct_timestamp.strftime(format)
-    print(start_delta)
+    print("HWM is greater than current timestamp, Assigning current timestamp as HWM")
+    end_delta = ct_timestamp.strftime(format)
+    print(end_delta)
 else:
-    print('New HWM is less than current timestamp, providing new hwm ')
-    start_delta = new_hwm_chge.strftime(format)
-    print(start_delta)
+    print('New HWM is less than current timestamp, Assigning New HWM ')
+    end_delta = new_hwm_chge.strftime(format)
+    print(end_delta)
